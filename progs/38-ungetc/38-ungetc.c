d7e3aaa6217bb944124b92a9d6d3d83467842549
c3f0 <c3f0@proton.me>
2023-11-28 08:19:37 +0100

    bunch of programs

---
+++ progs/38-ungetc/38-ungetc.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main()
+
+{
+  FILE *file;
+  file = fopen("file.txt", "r");
+  
+  if(file == NULL)
+    {
+      printf("Error opening file.\n");
+      return 1;
+    }
+
+  /* int c; */
+  /* while(true) */
+  /*   { */
+  /*     c = fgetc(file); */
+
+  /*     // if EOF break loop */
+  /*     if(feof(file) || ferror(file)) break; */
+
+  /*     // unget will move to where e is replace it with z and */
+  /*     // continue loop */
+  /*     if(c == 'e') ungetc('z', file); */
+  /*     else printf("%c", c); */
+            
+  /*   } */
+
+  int c;
+  char op1[256];
+  int i = 0;
+  while(true)
+    {
+      c = fgetc(file);
+      if(feof(file) || ferror(file)) break;
+      if(c == '+')
+        {
+          ungetc(c, file);
+          break;
+        }
+      else
+        {
+          op1[i] = c;
+          i++;
+        }
+    }
+  op1[i] = '\0';
+  printf("op1: %s\n", op1);
+  char rest[256];
+
+
+
+  printf("\n");
+  fclose(file);
+         
+  return 0;
+}
